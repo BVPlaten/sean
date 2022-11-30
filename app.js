6cec2148a5157f288f70c150bb0091f582f1a8df
Bernd von Platen <bernd.von.platen@gmail.com>
2022-11-30 09:31:52 +0100

    changing to OO part 1

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+app.use('/libs/', express.static(path.join(__dirname, 'node_modules/three/addons/libs')))
 
 app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
 
